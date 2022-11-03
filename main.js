c032eb6a9a47af9a327129c8aed2177de554c645
Chet Jogia <chetjogia@gmail.com>
2022-11-03 15:59:39 +0000

    completed codewarriors question and tests

--- main.js
+++ main.js
@@ -1,71 +1,25 @@
 /* 
 👉 Write your kata here!
-*///👉 Write your tests below here:
-//Number of hours till Deadline calculator assuming noon from starting day to noon on deadline day without using the DATE function 
 
-//Date and deadline given as a string e.g. 10th January 2022
+You've got some work to hand in but you're a little lazy and want to know how many days you have left until the deadline so you can leave it till the last minute! 
 
-//plan to solve, number of hours is, find number of days between both dates and then multiply by 24
-//Assume february has 28 days
-//Assume dates within same year
+- Write a function that takes in the date you are starting from in the format 'date month year', e.g. ('20th February 2022') and the date of the deadline ('5th June 2022'). 
+- The function must return the number of days you have left up till the deadline. You can assume you have the full day you are starting from, but as soon as the date changes to the deadline you have to hand in the work - so you can't include that day!
 
-
-function deadlineCalc(startDate, endDate){
-    let arrayOfMonthsAndDays = [['January', 31],['February', 28], ['March', 31], ['April', 30], ['May', 31], ['June', 30], ['July', 31], ['August', 31], ['September', 30], ['October', 31], ['November', 30], ['December', 31]]
-    let startDateArray = startDate.split(" ")
-    let endDateArray = endDate.split(" ")
-    let startMonth = startDateArray[1]
-    let endMonth = endDateArray[1]
-    let startDay = ""
-    let endDay = ""
-
-    
-    if(startDate[1]==='s'||startDate[1]==='r'||startDate[1]==='t'||startDate[1]==='n'){
-        startDay = startDateArray[0].substring(0,1);
-    }
-    else{
-        startDay = startDateArray[0].substring(0,2);
-    }
-
-    if(endDate[1]==='s'||endDate[1]==='r'||endDate[1]==='t'||endDate[1]==='n'){
-        endDay = endDateArray[0].substring(0,1);
-    }
-    else{
-        endDay = endDateArray[0].substring(0,2);
-    }
+Assumptions:
+- The dates will always lie within the same year (i.e. the start date can't be in 2022 and the deadline in 2023)
+- Assume there are no leap years and that February always has 28 days
 
 
-    let startIndex = 0;
-    let endIndex = 0;
-    for(let i=0; i<arrayOfMonthsAndDays.length; i++){
-        if(arrayOfMonthsAndDays[i][0]===startMonth){
-            startIndex = i;
-        }
-        if(arrayOfMonthsAndDays[i][0]===endMonth){
-            endIndex = i;
-        }
-    }
+*/
 
-    let answer = 0
-    for(let i = startIndex+1; i<endIndex; i++){
-        answer += arrayOfMonthsAndDays[i][1];
-        console.log(answer)
-    }
+//👉 Write the function your CodeWarriors will start with below here:
 
-    if(startMonth === endMonth){
-        answer += endDay - startDay
-    }
-    else{
-        answer += arrayOfMonthsAndDays[startIndex][1]-Number(startDay);
-        answer += Number(endDay);
-    }
-
-
-    console.log(answer)
+export function deadlineCalc(startDate, endDate){
+   
     return answer
 }
 
 
-deadlineCalc('6th January 2022', '10th January 2022')
 
-//👉 Write the function your CodeWarriors will start with below here:
+
