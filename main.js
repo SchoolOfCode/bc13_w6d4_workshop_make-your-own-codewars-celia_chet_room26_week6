b7d2449b037d92939e049c3d6a77033c9a5ea9f7
Chet Jogia <chetjogia@gmail.com>
2022-11-03 16:04:11 +0000

    Complete the codewarriors to earn kyu points

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* 
 👉 Write your kata here!
 
-You've got some work to hand in but you're a little lazy and want to know how many days you have left until the deadline so you can leave it till the last minute! 
+You've got some work to hand in but you're a little lazy and want to know how many days you have left until the deadline so you can leave it till the last minute!!
 
 - Write a function that takes in the date you are starting from in the format 'date month year', e.g. ('20th February 2022') and the date of the deadline ('5th June 2022'). 
 - The function must return the number of days you have left up till the deadline. You can assume you have the full day you are starting from, but as soon as the date changes to the deadline you have to hand in the work - so you can't include that day!
